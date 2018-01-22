f6155bd1d17dbb65a8be42df3d500d00fa3cc5d3
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-01-22 13:44:43 -0600

    working sample spring based ws. \n issue with running from intellij ide community version

--- src/main/kotlin/com/bhochhi/kafka/logger/KafkaLoggerApplication.kt
+++ src/main/kotlin/com/bhochhi/kafka/logger/KafkaLoggerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KafkaDemoApplication
+class KafkaLoggerApplication
 
 fun main(args: Array<String>) {
-    runApplication<KafkaDemoApplication>(*args)
+    runApplication<KafkaLoggerApplication>(*args)
 }
